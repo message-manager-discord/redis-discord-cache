e258cc7928b774e4497566e352f61aa2823e6171
Sam <anothercat1259@gmail.com>
2022-02-25 15:25:10 +1300

    chore: add guild to exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import GatewayClient from "./gateway";
 import { createRedisClient } from "./redisClient";
 import { clearCache } from "./utils";
 import GuildManager from "./guildManager";
-export { GatewayClient, createRedisClient, clearCache, GuildManager };
+import Guild from "./structures/guild";
+export { GatewayClient, createRedisClient, clearCache, GuildManager, Guild };
